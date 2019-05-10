88f7afd113b42c29a2466ab560a76a96f3bd3998
OyugiTolbert <oyugitolbert@gmail.com>
2019-05-10 21:35:19 +0300

    3rd commit

--- src/main/java/com/example/jobapp/Company_actions.java
+++ src/main/java/com/example/jobapp/Company_actions.java
@@ -1,13 +1,38 @@
 package com.example.jobapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Company_actions extends AppCompatActivity {
+    private Button jobview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company_actions);
+
+        jobview = findViewById(R.id.update_profilejob);
+
+        viewProfile();
+        viewJob();
+        viewApplicant();
+    }
+
+    private void viewJob() {
+    }
+
+    private void viewProfile() {
+        jobview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Company_actions.this,Job_Profile.class));
+            }
+        });
+    }
+    private void viewApplicant(){
+
     }
 }
